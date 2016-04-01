53b92ea570c3b72b9264063e087a0fcb21492271
joe199 <joe199ortiz@gmail.com>
2016-04-01 16:40:05 +0200

    he afegit quatre lineies, per provar

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ class database:
 
     def __init__(self):
         self.clients = dict()
-    
+
     def client(self, client, beer):
         if client in self.clients:
             self.clients[client] = self.clients[client] + beer
@@ -19,11 +19,14 @@ class database:
             self.clients[client] = beer
         print self.clients
         return self.clients[client]
-        
-    
+
+
     def tank(self, tank):
         self.tanki = tank
         tank = float(tank)-0.33
         tank = '%.2f' % (tank)
         return tank
 
+"He afegit comentari per aveure si puc fer un sol comit d'ixo"
+if a==b:
+    print hola
