8df31aaec6230893cc5dfe575efbb3ebf4186c9a
joe199 <joe199ortiz@gmail.com>
2016-04-19 11:49:31 +0200

    prova CommandList

---
+++ actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Action(object):
+    """docstring for command"""
+    def __init__(self):
+        super(command, self).__init__()
+        #self.arg = arg
+        print "Actions executed"
+
+class telegram(Action):
+    """docstring for telegram"""
+    def __init__(self):
+        super(telegram, self).__init__()
+        self.consult = ["telegram"]
+
+    def is_for_you(self, word):
+        if word in self.consult:
+            return True
+        return False
+
+    def do(self, command):
+        print "telegram done"
+        return "DONE"
+
+
+class nfc(Action):
+    """client beer updating"""
+    def __init__(self):
+        super(nfc, self).__init__()
+        self.consult = ["nfc"]
+
+    def is_for_you(self, word):
+        if word in self.consult:
+            return True
+        return False
+
+    def do(self, command):
+        print "nfc done"
+        return "OK"
---
+++ nfckeg_121.py
@@ -0,0 +1,56 @@
+import socket
+import sys
+import os
+import subprocess
+import time
+import threading
+import database
+
+beer = 0
+tank = 0.99
+d = database.database()
+
+class NFCKEG(threading.Thread):
+
+    def __init__(self, client):
+        threading.Thread.__init__(self)
+        self.client_sock, self.client_addr = client
+
+    def run(self):
+
+        global beer
+        global tank
+
+        while True:
+
+            #time.sleep(2)
+            value = self.client_sock.recv(1024)
+
+            if tank < 0.33:
+                    print 'Recharging the tank $$'
+                    value2 = 'Whait'
+                    self.client_sock.sendall(str(value2))
+                    time.sleep(2)
+                    tank = 1.65
+                    print 'Tank recharged!'
+
+            print '\nState 1 of the tank is %s L.' % tank
+            tanki = tank
+            tank = float(d.tank(tank))
+            beer = tanki - tank
+            tdrinked = d.client(value, beer)
+            print 'State 2 of the tank is %s L.' % tank
+            value2 = tdrinked
+            self.client_sock.sendall(str(value2))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('', 9999))
+sock.listen(3)
+print 'Waiting_for_drinkers_...'
+
+
+
+if __name__ == '__main__':
+    while True:
+        client = sock.accept()
+        NFCKEG(client).start()
