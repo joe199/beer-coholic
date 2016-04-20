738696461390adbcae923f0071c7fee42dfcc8c6
joelgodia93 <joelgodia@hotmail.com>
2016-04-20 19:26:42 +0200

    Telegram prova

---
+++ channels/TelegramChannel.py
@@ -0,0 +1,49 @@
+from channels import Channel
+
+import telepot
+
+
+class DuffmanBot(telepot.Bot):
+    """DuffmanBot is my telegram bot"""
+    def __init__(self, token):
+        super(DuffmanBot, self).__init__(token)
+        self.clist = None
+        self.chat_id = None
+
+    def set_list(self, clist):
+        self.clist = clist
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type == 'text':
+            command = msg['text']
+            if self.clist is not None:
+                self.clist.append(command)
+                self.chat_id = chat_id
+
+    def respond(self, response):
+        if self.chat_id is not None:
+            self.sendMessage(self.chat_id, response)
+
+
+
+class TelegramChannel(Channel):
+    """Channel class, received commands from telegram"""
+    def __init__(self, name="TelegramChannel"):
+        super(TelegramChannel, self).__init__(name)
+        self.bot = DuffmanBot("202437938:AAGN93XJmHFRQvYrsKy0HBI-vulnqtHSeeo")
+        self.prova_nfc= []
+        self.bot.set_list(self.prova_nfc)
+        self.bot.notifyOnMessage()
+
+    def get_msg(self):
+        if self.msg_avail():
+            return self.prova_nfc.pop(0)
+
+    def msg_avail(self):
+        return len(self.prova_nfc) > 0
+
+    def respond(self, response):
+        if response is None:
+            response = "Command not understood"
+        self.bot.respond(response)
---
+++ channels/__init__.py
@@ -0,0 +1,5 @@
+from channels import Channel
+from TelegramChannel import TelegramChannel
+
+
+__all__ = ["Channel", "TelegramChannel"]
