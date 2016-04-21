c11af726f57ea4410cfed72dc4e3c91395468ab1
joelgodia93 <joelgodia@hotmail.com>
2016-04-21 21:43:43 +0200

    Error TextChannel

--- channels/TextChannel.py
+++ channels/TextChannel.py
@@ -5,17 +5,17 @@ class TextChannel(Channel):
     """channel class, reads file channels"""
     def __init__(self, name = "TextChannel"):
         super(TextChannel, self).__init__(name)
-        self.messages = []
+        self.prova_nfc = []
         with open("prova_nfc.txt","r") as f:
             for line in f:
-                self.messages.append(line)
+                self.prova_nfc.append(line)
 
     def get_msg(self):
         if self.msg_avail():
-            return self.messages.pop(0)
+            return self.prova_nfc.pop(0)
 
     def msg_avail(self):
-        return len(self.messages) > 0
+        return len(self.prova_nfc) > 0
 
     def respond(self, response):
         print "Response: ", response
